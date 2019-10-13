7d9bc05bcba6a0e0784cbcfc9f4594f7941fa9bc
Yavuz Tas <yavuzslmtas@gmail.com>
2019-10-13 12:17:21 +0200

    clear up code

--- src/main/java/dev/yavuztas/samples/gson/generic/SingleAwareListTypeAdapterFactory.java
+++ src/main/java/dev/yavuztas/samples/gson/generic/SingleAwareListTypeAdapterFactory.java
@@ -8,8 +8,6 @@ import com.google.gson.TypeAdapter;
 import com.google.gson.TypeAdapterFactory;
 import com.google.gson.reflect.TypeToken;
 
-import dev.yavuztas.samples.gson.model.CommentModel;
-
 /**
  * Custom TypeAdapterFactory for GSON to handle {@link SingleAwareList}
  * converstion
@@ -19,24 +17,19 @@ import dev.yavuztas.samples.gson.model.CommentModel;
  */
 public class SingleAwareListTypeAdapterFactory implements TypeAdapterFactory {
 
-    @Override
-    public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
+	@Override
+	public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
 
-        // proceed only if the incoming type is a parameterized type
-        if (!ParameterizedType.class.isInstance(type.getType())) {
-            return null;
-        }
+		// proceed only if the incoming type is a parameterized type
+		if (!ParameterizedType.class.isInstance(type.getType())) {
+			return null;
+		}
 
-        ParameterizedType parameterizedType = ParameterizedType.class.cast(type.getType());
-        Type elementType = parameterizedType.getActualTypeArguments()[0];
-        Class rawElementType = Class.class.cast(elementType);
+		ParameterizedType parameterizedType = ParameterizedType.class.cast(type.getType());
+		Type elementType = parameterizedType.getActualTypeArguments()[0];
+		Class rawElementType = Class.class.cast(elementType);
 
-        // and proceed only if the element type is CommentModel
-        if (!rawElementType.equals(CommentModel.class)) {
-            return null;
-        }
-
-        return (TypeAdapter<T>) new SingleAwareListTypeAdapter(gson, rawElementType);
-    }
+		return (TypeAdapter<T>) new SingleAwareListTypeAdapter(gson, rawElementType);
+	}
 
 }
